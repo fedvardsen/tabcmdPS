bc3f7990e6248783413a19e706549d78b0c66fd8
Finn Erik Edvardsen <fedvardsen@tableau.com>
2018-06-07 13:55:57 -0700

    Removed "remove users" Option

--- add_delete_users.ps1
+++ add_delete_users.ps1
@@ -1,17 +1,18 @@
 ﻿# SYNOPSIS - Test Login - Finn Erik Edvardsen - Tableau
 # V 0.1.2 - Menu Option Added - 06.07.2018 Finn Erik
+# V 0.1.2 - Removed "remove users" Option - 06.07.2018 Finn Erik
 
 # Initialize Menu
 function Show-Menu
 {
      param (
-           [string]$Title = 'Visa Add, Remove or Delete Users'
+           [string]$Title = 'Visa Tableau Online: Add, Remove or Delete Users'
      )
      Clear-Host
      Write-Host "================ $Title ================"
 
      Write-Host "1: Add New users."
-     Write-Host "2: Remove Existing users."
+     Write-Host "2: Add users to group."
      Write-Host "3: Delete Existing users."
      Write-Host "Q: Press 'Q' to quit."
 }
@@ -22,7 +23,7 @@ $username='fedvardsen@tableau.com'
 $password='LiliLulu2016!'
 #$project='Visa SA Shadow'
 
-Set-Location "C:\Tabcmd\extras\Command Line Utility\"
+Set-Location "C:\Tabcmd\Command Line Utility\"
 start-process .\tabcmd "login -s $server -t fedvardsdata -u $username -p $password"
 
 do
@@ -33,16 +34,16 @@ do
      {
            '1' {
             Clear-Host
-                'Add New Users'
+                'Adding New Users'
                 start-process .\tabcmd "createsiteusers c:\scripts\input\users_test.csv"
            } '2' {
             Clear-Host
-                'Remove Existing Users'
-                start-process .\tabcmd "removeusers c:\scripts\input\remove_users.csv"
+                'Adding users to Group'
+                start-process .\tabcmd "addusers Development --users  c:\scripts\input\users_group.csv"
            } '3' {
             Clear-Host
                 'Delete Existing Users'
-                start-process .\tabcmd "deleteusers c:\scripts\input\delete_users.csv"
+                start-process .\tabcmd "deletesiteusers c:\scripts\input\delete_users.csv"
            } 'q' {
                 return
            }
@@ -75,7 +76,7 @@ until ($input -eq 'q')
 # Move-Item -Destination $archive
 
 # Remove Users
-start-process .\tabcmd "deleteusers c:\scripts\input\delete_users.csv"
+# start-process .\tabcmd "deleteusers c:\scripts\input\delete_users.csv"
 
 # Delete Users
 # start-process .\tabcmd "deleteusers c:\tabcmd\input\delete_users.csv"
